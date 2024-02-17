86cb37c81ab88e75462a812290fdf1dbe35566b9
weirdghost7 <rohangamer2912@gmail.com>
2024-02-17 12:29:20 +0530

    service

--- ServiceDis/serv_active.py
+++ ServiceDis/serv_active.py
@@ -1,6 +1,23 @@
-import nmap 
+from nmap import *
 
-sr_port=int(input('s: '))
-er_port=int(input('e: '))
+ip='127.0.0.1'
+prt=['90','8080']
+sr_prt=89
+er_prt=91
+def Scanner(ip,port):
+    scanner=PortScanner()
+    res=scanner.scan(ip,port)
+    scan_res=res['scan'][ip]['tcp']
+    return print(scan_res)
 
-sprt=[]
+def Specified_Scanner():
+    for port in prt:
+        Scanner(ip,port)
+
+def Range_Scanner():
+    for port in range(sr_prt,er_prt+1):
+        Scanner(ip,port.__str__())
+if __name__=="__main__":
+    # Specified_Scanner()
+    Range_Scanner()
+ 
